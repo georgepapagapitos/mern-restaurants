d448b4402d086f4c4a3d9b362f1a3f0c89fb4ec3
George Papagapitos <george.g.papagapitos@gmail.com>
2021-05-15 09:12:23 -0500

    wiring up frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AddReview from './components/AddReview';
@@ -20,57 +20,54 @@ function App() {
   }
 
   return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/restaurants" className="navbar-brand">
-            Restaurant Reviews
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
         </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/restaurants"} className="nav-link">
-                Restaurants
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/restaurants"} className="nav-link">
+              Restaurants
             </Link>
-            </li>
-            <li className="nav-item">
-              {user ? (
-                <a href="/" onClick={logout} className="nav-link" style={{ cursor: 'pointer' }}>
-                  Logout {user.name}
-                </a>
-              ) : (
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              )}
-            </li>
-          </div>
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
-            <Route
-              path="/restaurants/:id/review"
-              render={(props) => (
-                <AddReview {...props} user={user} />
-              )}
-            />
-            <Route
-              path="/restaurants/:id"
-              render={(props) => (
-                <Restaurant {...props} user={user} />
-              )}
-            />
-            <Route
-              path="/login"
-              render={(props) => (
-                <Login {...props} login={login} />
-              )}
-            />
-          </Switch>
+          </li>
+          <li className="nav-item">
+            {user ? (
+              <a href="/" onClick={logout} className="nav-link" style={{ cursor: 'pointer' }}>
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            )}
+          </li>
         </div>
+      </nav>
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
+          <Route
+            path="/restaurants/:id/review"
+            render={(props) => (
+              <AddReview {...props} user={user} />
+            )}
+          />
+          <Route
+            path="/restaurants/:id"
+            render={(props) => (
+              <Restaurant {...props} user={user} />
+            )}
+          />
+          <Route
+            path="/login"
+            render={(props) => (
+              <Login {...props} login={login} />
+            )}
+          />
+        </Switch>
       </div>
-    </Router>
+    </div>
   );
 }
 
--- frontend/src/components/RestaurantsList.jsx
+++ frontend/src/components/RestaurantsList.jsx
@@ -1,9 +1,164 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import RestaurantDataService from '../services/restaurant';
+import { Link } from 'react-router-dom';
 
-function RestaurantsList() {
+const RestaurantsList = props => {
+
+  const [restaurants, setRestaurants] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [searchZip, setSearchZip] = useState("");
+  const [searchCuisine, setSearchCuisine] = useState("");
+  const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+  useEffect(() => {
+    retrieveRestaurants();
+    retrieveCuisines();
+  }, []);
+
+  const onChangeSearchName = event => {
+    const searchName = event.target.value;
+    setSearchName(searchName);
+  };
+
+  const onChangeSearchZip = event => {
+    const searchZip = event.target.value;
+    setSearchZip(searchZip);
+  };
+
+  const onChangeSearchCuisine = event => {
+    const searchCuisine = event.target.value;
+    setSearchCuisine(searchCuisine);
+  };
+
+  const retrieveRestaurants = () => {
+    RestaurantDataService.getAll()
+      .then(response => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch(err => {
+        console.error(err.message);
+      })
+  };
+
+  const retrieveCuisines = () => {
+    RestaurantDataService.getCuisines()
+      .then(response => {
+        console.log(response.data);
+        setCuisines(["All Cuisines"].concat(response.data));
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  };
+
+  const refreshList = () => {
+    retrieveRestaurants();
+  };
+
+  const find = (query, by) => {
+    RestaurantDataService.find(query, by)
+      .then(response => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  };
+
+  const findByName = () => {
+    find(searchName, "name");
+  };
+
+  const findByZip = () => {
+    find(searchZip, "zipcode");
+  };
+
+  const findByCuisine = () => {
+    if (searchCuisine == "All Cuisines") {
+      refreshList();
+    } else {
+      find(searchCuisine, "cuisine");
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>Hello, world</h1>
+    <div>
+      <div className="row pb-1">
+        <div className="input-group col-lg-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by name"
+            value={searchName}
+            onChange={onChangeSearchName}
+          />
+          <div className='input-group-append'>
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByName}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="input-group col-lg-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search by zip"
+            value={searchZip}
+            onChange={onChangeSearchZip}
+          />
+          <div className='input-group-append'>
+            <button
+              className='btn btn-outline-secondary'
+              type="button"
+              onClick={findByZip}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="input-group col-lg-4">
+          <select onChange={onChangeSearchCuisine}>
+            {cuisines.map(cuisine => {
+              return (
+                <option value={cuisine}>{cuisine.substr(0, 20)}</option>
+              )
+            })}
+          </select>
+          <div className='input-group-append'>
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByCuisine}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        {restaurants.map((restaurant) => {
+          const address = `${restaurant.address.building} ${restaurant.address.street} ${restaurant.address.zipcode}`;
+          return (
+            <div className="col-lg-4 pb-1">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{restaurant.name}</h5>
+                  <p className="card-text">
+                    <strong>Cuisine: </strong>{restaurant.cuisine}<br />
+                    <strong>Address: </strong>{address}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
